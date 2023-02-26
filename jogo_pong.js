53a8c8d0e1c643b370a8510f64c12816e8145f17
igor-hudson <125969022+igor-hudson@users.noreply.github.com>
2023-02-25 23:32:12 -0300

    Projeto quase finalizado

--- jogo_pong.js
+++ jogo_pong.js
@@ -1,39 +1,111 @@
-  // Posição da bolinha
-  let xBolinha = 300;
-  let yBolinha = 200;
-  let dBolinha = 20;
-  let rBolinha = dBolinha / 2;
-  
-    // Velocidade da bolinha
-  let velocidade_xBolinha = 4;
-  let velocidade_yBolinha = 2;
-  
-  function setup() {
-    createCanvas(600, 400);
+// Posição da bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let dBolinha = 20;
+let rBolinha = dBolinha / 2;
+
+// Velocidade da bolinha
+let velocidade_xBolinha = 6;
+let velocidade_yBolinha = 6;
+
+//variáveis da raquete
+let xRaquete = 5
+let yRaquete = 150
+let largura_Raquete = 10
+let altura_Raquete = 90
+
+//variáveis da raquete do oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+let velocidade_yRaqueteOponente;
+
+//placar do jogo
+let pontosMeus = 0;
+let pontosOponente = 0;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+// Objetos do cenário
+function draw() {
+  background(0);
+  mostraBolinha();
+  velocidadeBolinha();
+  verificaColisaoBorda();
+  mostraRaquete(xRaquete,yRaquete);
+  movimentaRaquete();
+  verificaColisaoRaquete();
+  mostraRaquete(xRaqueteOponente,yRaqueteOponente);
+  movimentoRaqueteOponente();
+  verificaColisaoRaqueteOponente();
+  incluiPlacar();
+  marcaPonto();
+}
+
+function mostraBolinha(){
+  circle(xBolinha,yBolinha,dBolinha)
+}
+
+function velocidadeBolinha(){
+  xBolinha += velocidade_xBolinha
+  yBolinha += velocidade_yBolinha
+}
+
+function verificaColisaoBorda(){
+  if (xBolinha + rBolinha > width || xBolinha - rBolinha < 0){
+    velocidade_xBolinha *= -1;
   }
-  
-  // Objetos do cenário
-  function draw() {
-    background(0);
-    mostraBolinha();
-    velocidadeBolinha();
-    verificaColisaoBorda();
-    }
-  
-  function mostraBolinha(){
-    circle(xBolinha,yBolinha,dBolinha)
+  if(yBolinha + rBolinha > height || yBolinha - rBolinha < 0){
+    velocidade_yBolinha *= -1;
   }
-  
-  function velocidadeBolinha(){
-    xBolinha += velocidade_xBolinha
-    yBolinha += velocidade_yBolinha
+}
+
+function mostraRaquete(x,y){
+  rect(x, y, largura_Raquete, altura_Raquete)
+}
+
+function movimentaRaquete(){
+  if (keyIsDown(UP_ARROW)){
+    yRaquete -= 10
   }
-  
-  function verificaColisaoBorda(){
-    if (xBolinha + rBolinha > width || xBolinha - rBolinha < 0){
-      velocidade_xBolinha *= -1;
-    }
-    if(yBolinha + rBolinha > height || yBolinha - rBolinha < 0){
-      velocidade_yBolinha *= -1;
-    }
+  if (keyIsDown(DOWN_ARROW)){
+    yRaquete += 10
   }
+}
+
+function verificaColisaoRaquete(){
+  if (xBolinha - rBolinha < xRaquete + largura_Raquete &&
+      yBolinha - rBolinha < yRaquete + altura_Raquete &&
+      yBolinha + rBolinha > yRaquete){
+    velocidade_xBolinha *= -1
+  }
+}
+function verificaColisaoRaqueteOponente(){
+  if (xBolinha + rBolinha > xRaqueteOponente - largura_Raquete &&
+     yBolinha - rBolinha < yRaqueteOponente + altura_Raquete &&
+     yBolinha + rBolinha > yRaqueteOponente){
+    velocidade_xBolinha *= -1
+  }
+}
+function movimentoRaqueteOponente(){
+  velocidade_yRaqueteOponente = yBolinha - yRaqueteOponente - largura_Raquete / 2 - 30;
+  yRaqueteOponente += velocidade_yRaqueteOponente
+}
+
+function incluiPlacar(){
+  textAlign(CENTER);
+  textSize(16);
+  fill(255)
+  text(pontosMeus, 150, 25);
+  text(pontosOponente, 450, 25);
+}
+
+function marcaPonto(){
+  if (xBolinha + rBolinha > 599){
+    pontosMeus += 1
+  }
+  if (xBolinha - rBolinha < 1){
+    pontosOponente += 1
+  }
+}
